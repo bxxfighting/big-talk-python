57d9f585c48bc52398490421cd47ff2f78f35b7d
卜星星 <boxingxing@limikeji.com>
2020-12-12 17:17:50 +0800

    sleep

---
+++ 小故事/小明请保姆/05.py
@@ -0,0 +1,66 @@
+from time import sleep
+
+
+class Person:
+    '''
+    基础类
+    '''
+
+    def __init__(self, name):
+        self.name = name
+
+
+class Employer(Person):
+    '''
+    雇主类
+    '''
+
+    def call_cleaners(self, count):
+        clean_company = CleanCompany('爱干净')
+        print(self.name, '叫了', clean_company.name, '公司的', count, '个保洁')
+        clean_company.clean_order(self, 10)
+
+
+class CleanCompany:
+    '''
+    保洁公司
+    '''
+
+    def __init__(self, name):
+        self.name = name
+
+    def clean_order(self, employer, count):
+        '''
+        保洁订单
+        '''
+        self.assign_cleaners(employer, count)
+
+    def assign_cleaners(self, employer, count):
+        '''
+        分配保洁
+        '''
+        for i in range(count):
+            cleaner = Cleaner(f'保洁员{i+1}号')
+            cleaner.go_to_work(employer)
+
+
+class Cleaner(Person):
+    '''
+    保洁员类
+    '''
+
+    def go_to_work(self, employer):
+        print(self.name, '正在前往', employer.name, '的家')
+        sleep(2)
+        print(self.name, '正在前往', employer.name, '的家')
+        self.clean(employer)
+
+    def clean(self, employer):
+        print(self.name, '正在打扫', employer.name, '的家')
+        sleep(2)
+        print(self.name, '打扫完成', employer.name, '的家')
+
+
+if __name__ == '__main__':
+    xiaoming = Employer('小明')
+    xiaoming.call_cleaners(10)
